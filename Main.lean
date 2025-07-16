1c56bd1b63ff8bddaff13a2e65382de9479bc7e1
ZMTJQ <150300773+ZMTJQ@users.noreply.github.com>
2025-07-16 17:26:20 +0200

    Update Main.lean

--- Main.lean
+++ Main.lean
@@ -8,7 +8,7 @@ open Lean IO System Elab Command Meta Lean.Meta Lean.Elab.Tactic
 def agPkgPathPrefix : FilePath := ".lake" / "packages" / "autograder"
 def solutionDirName := "AutograderTests"
 def submissionUploadDir : FilePath := "/autograder/submission"
-def resultsJsonPath : FilePath := ".." / "results" / "results.json"
+def resultsJsonPath : FilePath := "." / "results" / "results.json"
 -- These are arbitrary
 def solutionModuleName := "Solution"
 def submissionFileName := "Assignment.lean"
